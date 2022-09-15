41e41af3e4cf6cf33927185cc9377711b54244e6
Parallels <154630877@qq.com>
2022-09-15 22:00:13 +0800

    深圳包

--- main.py
+++ main.py
@@ -21,12 +21,5 @@ if __name__ == '__main__':
     print(a)
 
     jd_seckill = JdSeckill()
-    choice_function = input('请选择:')
-    if choice_function == '1':
-        jd_seckill.reserve()
-    elif choice_function == '2':
-        jd_seckill.seckill_by_proc_pool()
-    else:
-        print('没有此功能')
-        sys.exit(1)
+    jd_seckill.seckill_by_proc_pool()
 
